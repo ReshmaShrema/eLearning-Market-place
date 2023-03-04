742e94f67e86290059393d00a57cbb83df535504
ReshmaShrema <shremareshma@gmail.com>
2023-03-04 22:56:24 +0530

    create registration page

--- client/pages/register.js
+++ client/pages/register.js
@@ -1,10 +1,49 @@
+import { useState } from 'react';
+
 const Register = () => {
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleSubmit = (e) => {
+        // call when submit the event
+        // prevent the page reloading when the page is submitted
+        e.preventDefault();
+        console.table({name,email,password})
+    };
     return (
         <>
-            <h1 className="jumbotron">
-                Register
-            </h1>
-            <p>Register page</p>
+            <h1 className="jumbotron">Register</h1>
+            {/* col-md-4 offset-md-4 make it in center */}
+            <div className="container col-md-4 offset-md-4 pb-5">
+                <form onSubmit={handleSubmit}>
+                    <input
+                        type="text"
+                        className="form-control mb-4 p-4"
+                        value={name}
+                        onChange={(e) => setName(e.target.value)}
+                        placeholder="Enter Name"
+                        required
+                    />
+                    <input
+                        type="email"
+                        className="form-control mb-4 p-4"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        placeholder="Enter Email"
+                        required
+                    />
+                    <input
+                        type="password"
+                        className="form-control mb-4 p-4"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        placeholder="Enter Password"
+                        required
+                    />
+                    <button className='btn btn-block btn-primary' type='submit'>Submit</button>
+                </form>
+            </div>
         </>
     );
 };
--- readme.txt
+++ readme.txt
@@ -161,3 +161,56 @@ const TopNav = ()=>{
 export default TopNav;
 
 009
+create registration form
+import { useState } from 'react';
+
+const Register = () => {
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleSubmit = (e) => {
+        // call when submit the event
+        // prevent the page reloading when the page is submitted
+        e.preventDefault();
+        console.table({name,email,password})
+    };
+    return (
+        <>
+            <h1 className="jumbotron">Register</h1>
+            {/* col-md-4 offset-md-4 make it in center */}
+            <div className="container col-md-4 offset-md-4 pb-5">
+                <form onSubmit={handleSubmit}>
+                    <input
+                        type="text"
+                        className="form-control mb-4 p-4"
+                        value={name}
+                        onChange={(e) => setName(e.target.value)}
+                        placeholder="Enter Name"
+                        required
+                    />
+                    <input
+                        type="email"
+                        className="form-control mb-4 p-4"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        placeholder="Enter Email"
+                        required
+                    />
+                    <input
+                        type="password"
+                        className="form-control mb-4 p-4"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        placeholder="Enter Password"
+                        required
+                    />
+                    <button className='btn btn-block btn-primary' type='submit'>Submit</button>
+                </form>
+            </div>
+        </>
+    );
+};
+export default Register;
+
+010
